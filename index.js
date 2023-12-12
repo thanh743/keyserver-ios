1d4538adedd4105507ec97444169cfe3b2e0298b
Mac <mac@Macs-MacBook-Pro.local>
2023-12-12 10:11:02 +0700

    zz

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = require("path");
 
 app.use(express.static('public'))
 app.set('view engine' , 'ejs');
-
+console.log("Welcome to my express application!");
 // app.set('views', path.join(__dirname, './views'))
 
 // require("./startup/logging")();
