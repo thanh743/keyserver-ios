f6a966f924af73027a71d9ffc4a2115378a754e9
Mac <mac@Macs-MacBook-Pro.local>
2023-12-18 10:35:34 +0700

    zz

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,10 +3,11 @@ const config = require("config");
 
 module.exports = function(req, res, next) {
   if (!config.get("requiresAuth")) return next();
-  const token = req.headers.cookie.replace("X-Auth-Token=","");
-  if (!token) return res.status(401).send("Access denied. No token provided.");
+
 
   try {
+    const token = req.headers.cookie.replace("X-Auth-Token=","");
+    if (!token) return res.status(401).send("Access denied. No token provided.");
     const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
     req.user = decoded;
     next();
